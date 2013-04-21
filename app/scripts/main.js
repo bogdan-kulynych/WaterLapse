e66ffd5739d8d0e32a93b5877bce6a0790926d73
bogdan-kulynych <bogdan.kulinich@gmail.com>
2013-04-21 11:49:49 +0200

    Jquerized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,50 +4,53 @@
   xhr = new XMLHttpRequest();
   xhr.open('GET', datasource, true);
 
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        // var ungzipped = JXG.decompress(xhr.responseText);
-        var ungzipped = xhr.responseText;
-        var data = JSON.parse(ungzipped);
-        window.data = data;
+  $.getJSON(datasource, function(data) {
+    window.data = data;
 
-        var ratio = 1 / 2;
-        window.width = window.innerWidth,
-        window.height = ratio * width;
+    var ratio = 1 / 2;
+    window.width = window.innerWidth,
+    window.height = ratio * width;
 
-        var canvas = $('<canvas></canvas>')
-            .addClass('map')
-            .attr("width", width)
-            .attr("height", height);
-        $("body").append(canvas);
+    var canvas = $('<canvas></canvas>')
+        .addClass('map')
+        .attr("width", width)
+        .attr("height", height);
+    $("body").append(canvas);
 
-        var ctx = canvas[0].getContext("2d");
-        console.log(ctx);
+    var ctx = canvas[0].getContext("2d");
+    console.log(ctx);
 
-        plot_array(ctx, data[0]['data']);
-      }
-    };
-  };
-  xhr.send();
+    plot_array(ctx, data[0]['data']);
+  });
 
+  $('body').bind('mousemove click', function() {
+    var hide_timeout = 500;
+    var fade_timeout = 500;
+
+    $('#slider').fadeIn(fade_timeout);
+    clearTimeout(window.slider_timeout);
+    window.slider_timeout = setTimeout(function() {
+      console.log('slider hides now');
+      $('#slider').fadeOut(fade_timeout);
+    }, hide_timeout);
+  });
+
+  // http://stackoverflow.com/questions/14034455/translating-lat-long-to-actual-screen-x-y-coordinates-on-a-equirectangular-map
   var equirectangular = function(latitude, longitude) {
     x = ((longitude + 180) * (width  / 360));
     y = (((latitude * -1) + 90) * (height/ 180));
     return [x, y]
   };
 
+  // http://stackoverflow.com/questions/14329691/covert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection
   var mercator = function(latitude, longitude) {
     x = (width*(180+longitude)/360)%width+(width/2);
-    const PI = Math.PI;
-    const tan = Math.tan;
-    const log = Math.log;
 
     // convert from degrees to radians
-    latRad = latitude*PI/180;
+    latRad = latitude*Math.PI/180;
 
     // get y value
-    mercN = log(tan((PI/4)+(latRad/2)));
+    mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)));
     y     = (height/2)-(width*mercN/(2*PI));
     return [x - width / 2, y]
   };
